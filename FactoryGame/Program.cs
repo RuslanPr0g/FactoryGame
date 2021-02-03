a7f9201d740038562ea0018dfa0505091546849c
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-03 21:36:53 +0300

    add random helper

--- FactoryGame/Program.cs
+++ FactoryGame/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FactoryGame.Factory.Factories;
+using System;
+using System.Collections.Generic;
 
 namespace FactoryGame
 {
---
+++ RandomHelder/RngProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomHelper
+{
+    public static class RngProvider
+    {
+        public static Random Random { get; }
+
+        static RngProvider()
+        {
+            Random = new Random();
+        }
+    }
+}
